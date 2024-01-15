909d62615b14e056f029630f3011fe355216ea87
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-01-15 07:31:05 +0700

    first commit

---
+++ config/database.php
@@ -0,0 +1,8 @@
+<?php
+// deklarasi parameter koneksi database
+$sql_details = array(
+    'user' => 'root',                               // username database, default “root”
+    'pass' => 'root',                                   // password database, default kosong
+    'db'   => 'crudv3',                // nama database yang akan digunakan
+    'host' => 'localhost'                           // server database, default “localhost” atau “127.0.0.1”
+);
